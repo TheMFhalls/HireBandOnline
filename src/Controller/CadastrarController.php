249d395b35da04f174bc0c57003fd6c8dc0f5244
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2018-06-20 22:27:01 -0300

    Qua Jun 20 22:27:01 -03 2018

--- src/Controller/CadastrarController.php
+++ src/Controller/CadastrarController.php
@@ -4,16 +4,29 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/cadastrar")
+ */
 class CadastrarController extends Controller
 {
     /**
-     * @Route("/cadastrar", name="cadastrar")
+     * @Route("/", name="cadastrar", methods="GET")
      */
     public function index()
     {
         return $this->render('cadastrar/index.html.twig', [
             'controller_name' => 'CadastrarController',
         ]);
+    }
+
+
+    /**
+     * @Route("/new", name="cadastrar_new", methods="POST")
+     */
+    public function new(Request $request)
+    {
+
     }
 }
